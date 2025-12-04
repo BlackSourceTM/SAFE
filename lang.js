6f965974b6d58747aab1969829ddae5bea170d25
Black Source TM <110764527+BlackSourceTM@users.noreply.github.com>
2025-12-04 11:12:00 +0330

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,22 +1,25 @@
 (function(w){
 w.SAFE_LANG={en:{
-  modal_encrypt_title:"New encryption",
-  modal_decrypt_title:"New decryption",
-  field_file_label:"File",
-  field_password_label:"Password",
-  field_password_confirm_label:"Repeat password",
-  btn_start:"Start",
-  btn_cancel:"Cancel",
-  status_ready:"Ready",
-  status_working:"Working...",
-  status_done:"Done",
-  status_error:"Error",
-  validation_file_required:"Please select a file.",
-  validation_pwd_short:"Password must be at least 8 characters.",
-  validation_pwd_mismatch:"Passwords do not match.",
-  toast_encrypt_success:"File encrypted successfully.",
-  toast_decrypt_success:"File decrypted successfully.",
-  toast_operation_failed:"Operation failed. Please check password and file."
+modal_encrypt_title:"New encryption",
+modal_decrypt_title:"New decryption",
+field_file_label:"File",
+field_password_label:"Password",
+field_password_confirm_label:"Repeat password",
+btn_start:"Start",
+btn_cancel:"Cancel",
+status_ready:"Ready",
+status_working:"Working...",
+status_done:"Done",
+status_error:"Error",
+validation_file_required:"Please select a file.",
+validation_pwd_short:"Password must be at least 8 characters.",
+validation_pwd_mismatch:"Passwords do not match.",
+toast_encrypt_success:"File encrypted successfully.",
+toast_decrypt_success:"File decrypted successfully.",
+toast_operation_failed:"Operation failed. Please check password and file.",
+sheet_rules_title:"Usage rules",
+sheet_privacy_title:"Privacy policy",
+sheet_faq_title:"FAQ"
 }};
 w.SAFE_I18N=w.SAFE_LANG.en;
 })(window);
